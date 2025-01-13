93cb22635443c278618d909ffcad64707553793c
Brunna Carvalho <114610031+brunnarayssa@users.noreply.github.com>
2025-01-13 20:08:34 +0000

    subiendocodigo

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ToDo from "./ToDo.jsx"; // Ruta relativa
+
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<ToDo/>
+		</div>
+	);
+};
+
+export default Home;
